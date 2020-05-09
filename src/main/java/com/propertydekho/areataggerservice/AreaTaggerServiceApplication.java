f01c6ba4aa49987c540c558b8daeb0c6bd30019f
Divsjains6021 <divsjains6021@gmail.com>
2020-05-10 00:10:43 +0530

    Property Area Tagger Service

---
+++ src/main/java/com/propertydekho/areataggerservice/AreaTaggerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propertydekho.areataggerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreaTaggerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreaTaggerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertydekho/areataggerservice/AreaTaggerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertydekho.areataggerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreaTaggerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
